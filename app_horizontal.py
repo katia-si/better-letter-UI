c7730f2c79bb8aec54d99f480cade966d8d91d24
Hrvoje Santek <santekh@posteo.de>
2024-05-29 16:53:26 +0200

    added .python.version, -streamlit(, app_horizontal.py

--- app_horizontal.py
+++ app_horizontal.py
@@ -1,20 +1,22 @@
 import streamlit as st
 from PIL import Image
 
-
 # Create titles
 st.markdown("""# 	:office: :incoming_envelope: :rainbow-background[Better Letter] :love_letter: :love_hotel:
 ## :violet-background[Summarize Official German Letters]
 """)
 
 # Allow user to select number of lines via slider
-# line_count = st.slider('How many lines shall the summary have? ', 1, 10, 3)
+line_count = st.slider('How many lines shall the summary have? ', 1, 10, 3)
 
-# Allow user to upload several images
-uploaded_images = st.file_uploader("You want to upload your photos of the letter?", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+# Create columns for file uploader and camera input
+col1, col2 = st.columns(2)
 
-# enable use of user webcam
-enable_webcam = st.camera_input('You want to use your webcam?', help)
+with col1:
+    uploaded_images = st.file_uploader("Upload your photos of the letter?", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+
+with col2:
+    enable_webcam = st.camera_input('Use your webcam?')
 
 # Display uploaded images and summary
 if uploaded_images:
