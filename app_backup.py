79c40f2e862cf00b7036b8a1de34c5ecebe2ca20
Hrvoje Santek <santekh@posteo.de>
2024-06-05 16:23:23 +0200

    enabled camera processing; sidebar no default anymore

--- app_backup.py
+++ app_backup.py
@@ -4,31 +4,29 @@ from components import display_uploaded_image, display_summary
 import requests
 import time
 
-url = 'https://lighttesseract-new-4ipo33uimq-ew.a.run.app/summary_eng'
-
-def toggle_webcam():
-    st.session_state.webcam_enabled = not st.session_state.webcam_enabled
-
-
-# Set page configuration to hide the sidebar by default
-st.set_page_config(
-    page_title="Better Letter",
-    page_icon=":incoming_envelope:",
-    layout="centered",
-    initial_sidebar_state="collapsed",  # Hide sidebar by default
-)
+#url = 'http://0.0.0.0:8000/summary_eng'
+#url = 'https://light-ooqm7vogaa-ew.a.run.app/summary_eng'
+# url = 'https://better-letter-api-4ipo33uimq-ew.a.run.app'
+# url = 'https://lighttesseract-4ipo33uimq-uc.a.run.app/summary_eng'
+url = 'https://lighttesseract-new-4ipo33uimq-ew.a.run.app//summary_eng'
 
 # Initialize session state for webcam
 if 'webcam_enabled' not in st.session_state:
     st.session_state.webcam_enabled = False
 
 # Create titles
-st.markdown("# :incoming_envelope: BETTER LETTER")
-st.markdown("----------")
-st.markdown("### Got a letter in German language?  Let's figure out what it says!")
+#st.markdown("""# :office: :incoming_envelope: :rainbow-background[Better Letter] :love_letter: :love_hotel:
+## :violet-background[Summarize Official German Letters]
 
-# Sidebar
+# Create titles
+st.markdown("""# :incoming_envelope: BETTER LETTER """)
+st.markdown(""" ---------- """)
+st.markdown(""" ### Got a letter in German language?  Let's figure out what it says!""")
+
+
+# sidebar
 st.sidebar.image('https://postimg.cc/LJcdQtDk/deab52f9', use_column_width=True)
+
 
 # Allow user to upload image
 uploaded_image = st.file_uploader("You want to upload your photos of the letter?", type=["png", "jpg", "jpeg"], accept_multiple_files=False)
@@ -62,10 +60,25 @@ def process_image(image):  # New function to process both types of images
         response = requests.post(url, files=files).json()
         display_summary(response['summary'])
 
-# Process uploaded image
+# Display progress bar after uploading image
 if uploaded_image:
-    process_image(uploaded_image)  # Process uploaded image
+    uploaded_image_dict = {str(i): file for i, file in enumerate(uploaded_image)}
 
-# Process webcam image
-if st.session_state.webcam_enabled and webcam_image:
-    process_image(webcam_image)  # Process webcam image
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+
+    time.sleep(1)  # Simulate some work being done
+    my_bar.progress(50, text="Halfway done...")
+
+    time.sleep(1)  # Simulate more work being done
+    my_bar.progress(100, text="Operation complete.")
+
+    # Display uploaded image and summary
+    display_uploaded_image(uploaded_image)
+
+    # request to API
+    img = uploaded_image.getvalue()
+    files = {'file': img}
+    response = requests.post(url, files=files).json()
+    #display_summary(response)
+    display_summary(response['summary'])
